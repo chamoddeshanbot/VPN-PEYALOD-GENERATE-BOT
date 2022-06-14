c265e22604b533d879e01b39deb1aee51bffa9b8
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-06-14 13:28:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -222,7 +222,7 @@ async def id(client, message):
 @app.on_message(filters.command("username"))
 async def username(client, message):
     await message.reply_chat_action("typing")
-    text =f"G u name : {message.chat.username} You U name : {message.from.user.username}"
+    text =f"G u name : {message.chat.username} You U name : {message.from_user.username}"
     photo = get(f"https://single-developers.up.railway.app/logo?name={text}").history[1].url
     await message.reply_chat_action("upload_photo")
     await app.send_photo(message.chat.id,
