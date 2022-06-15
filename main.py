fb0cf9b515a8dd413af7e351adf4048e246b5213
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-06-15 10:28:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ async def exe(client, message):
        text = message.text.split(None, 1)[1]
        id = {text}
        cap =f"You Pyrogram Data Is Done {id}"
-       await app.send_photo(message.chat.id, caption=cap)
+       await app.send_photo(message.chat.id, photo=f"https://telegra.ph/file/50b455f8692db9c198a70.jpg", caption=cap)
     except Exception as e:
         await message.reply(str(e))
 
