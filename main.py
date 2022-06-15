78a5f34733066c755e8b9513d578d79cc2886ecb
chamoddeshanbot <95760031+chamoddeshanbot@users.noreply.github.com>
2022-06-15 10:10:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ async def help(client, message):
 async def exe(client, message):
     try:
        text = message.text.split(None, 1)[1]
-       await message.reply(text)
+       await message.reply(chat_id=text)
     except Exception as e:
         await message.reply(str(e))
 
